b70cbc77d0f9862a6668e742a4f8733efdd5df1b
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-01-26 21:54:16 +0600

    Add HeroSection component with text and banner image in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Fragment } from "react"
 import Navbar from "./Navbar"
+import HeroSection from "./HeroSection"
 
 const App = () => {
   return (
     <Fragment>
 
       <Navbar />
+
+      <HeroSection />
 
     </Fragment>
   )
