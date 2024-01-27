089677f48426fbd3bf3cb87a826d97f82ff7cf04
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-01-27 09:40:41 +0600

    Create context api and pass data in TaskBoard to TaskList component and add dynamic value

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -3,8 +3,20 @@ import Searchbox from "./Searchbox"
 import TaskActions from "./TaskActions"
 import TaskList from "./TaskList"
 import AddTaskModal from "./AddTaskModal"
+import { TaskContext } from "../context"
 
 const TaskBoard = () => {
+
+    const defaultTask = {
+        id: crypto.randomUUID(),
+        title: "Integration API",
+        description: "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+        tags: ['Web', 'Python', 'Api'],
+        priority: "High",
+        isFavourite: true
+    }
+
+    const [tasks, setTasks] = useState([defaultTask])
 
     const [showAddTaskModal, setShowAddTaskModal] = useState(false)
 
@@ -13,29 +25,33 @@ const TaskBoard = () => {
     return (
         <Fragment>
 
-            {showAddTaskModal && <AddTaskModal onCancelAddTaskModal={()=>setShowAddTaskModal(false)} />}
-            <section className="mb-20" id="tasks">
+            <TaskContext.Provider value={{tasks,setTasks}}>
 
-                <div className="container">
+                {showAddTaskModal && <AddTaskModal onCancelAddTaskModal={() => setShowAddTaskModal(false)} />}
+                <section className="mb-20" id="tasks">
+
+                    <div className="container">
 
 
-                    <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                        <div className="mb-14 items-center justify-between sm:flex">
-                            <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
-                            <div className="flex items-center space-x-5">
-                                {/* start searchbar */}
-                                <Searchbox />
+                        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+                            <div className="mb-14 items-center justify-between sm:flex">
+                                <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
+                                <div className="flex items-center space-x-5">
+                                    {/* start searchbar */}
+                                    <Searchbox />
 
-                                {/* Task Actions */}
-                                <TaskActions onAddTaskModal={()=>setShowAddTaskModal(true)} />
+                                    {/* Task Actions */}
+                                    <TaskActions onAddTaskModal={() => setShowAddTaskModal(true)} />
 
+                                </div>
                             </div>
+                            {/* Tasklist */}
+                            <TaskList />
                         </div>
-                        {/* Tasklist */}
-                        <TaskList />
                     </div>
-                </div>
-            </section>
+                </section>
+
+            </TaskContext.Provider>
 
         </Fragment>
     )
