93775cebf6c1f613ac0024dd76bce6269bcd4dd2
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-01-27 20:52:00 +0600

    Capitalize priority select text

--- src/Task/AddTaskModal.jsx
+++ src/Task/AddTaskModal.jsx
@@ -136,9 +136,9 @@ const AddTaskModal = () => {
                                 required
                             >
                                 <option value="">Select Priority</option>
-                                <option value="low">Low</option>
-                                <option value="medium">Medium</option>
-                                <option value="high">High</option>
+                                <option value="Low">Low</option>
+                                <option value="Medium">Medium</option>
+                                <option value="High">High</option>
                             </select>
                         </div>
                     </div>
